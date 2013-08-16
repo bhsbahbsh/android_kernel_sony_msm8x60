fb0fc04f834f2b6660ab3e945b13705520750619
RaymanFX <raymanfx@gmail.com>
2013-08-17 01:53:07 +0200

    msm_fb: fixup the merge

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -1937,7 +1937,6 @@ static int msm_fb_pan_idle(struct msm_fb_data_type *mfd)
 	mutex_lock(&mfd->sync_mutex);
 	if (mfd->is_committing) {
 		mutex_unlock(&mfd->sync_mutex);
-				&mfd->commit_comp,
 		ret = wait_for_completion_interruptible_timeout(
 				&mfd->commit_comp,
 			msecs_to_jiffies(WAIT_DISP_OP_TIMEOUT));
